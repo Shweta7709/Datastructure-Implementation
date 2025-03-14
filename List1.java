4a62226b18de175a9c00ce0d7a487ab6e96c429d
Shweta7709 <shwetannashte7709@gmail.com>
2025-03-14 14:33:35 +0530

    ArrayList code

---
+++ List1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class List1{
+public static void main(String[] args){
+
+ArrayList<String> list = new ArrayList<String>();
+list.add("Ravi");
+list.add("Vijay");
+list.add("Ravi");
+list.add("Ravi");
+
+
+System.out.println(list);
+}
+}
